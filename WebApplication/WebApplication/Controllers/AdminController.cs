e447e92cba8276b4f4535e2638005b15ea037ac0
TuanVu321 <tuantnmt1@gmail.com>
2020-09-14 14:30:00 +0700

    tra ve role cua user

--- WebApplication/WebApplication/Controllers/AdminController.cs
+++ WebApplication/WebApplication/Controllers/AdminController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
 
 namespace WebApplication.Controllers
 {
@@ -7,12 +11,28 @@ namespace WebApplication.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public AdminController(DataContext context)
+        {
+            _context = context;
+        }
+
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("usersWithRoles")]
-        
-        public IActionResult GetUsersWithRole()
+        public async Task<IActionResult> GetUsersWithRole()
         {
-            return Ok("Only admins can see this");
+            var userList = await _context.Users
+                .OrderBy(x => x.Username)
+                .Select(user => new
+                {
+                    Id = user.Id,
+                    UserName = user.Username,
+                    Roles = (from userRole in user.UserRoles join role in _context.Roles on userRole.RoleId equals role.Id
+                        select role.Name).ToList()
+                }).ToListAsync();
+            return Ok(userList);
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
